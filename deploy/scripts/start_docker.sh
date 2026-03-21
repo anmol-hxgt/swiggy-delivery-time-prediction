0ce9e489499ef0c146b4754663c2c42371de5d04
Anmol Chandra Mishra <anmolchandra871@gmail.com>
2026-03-21 15:03:22 +0530

    ci: add CodeDeploy steps

---
+++ deploy/scripts/start_docker.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Log everything to start_docker.log
+exec > /home/ubuntu/start_docker.log 2>&1
+
+echo "Logging in to ECR..."
+aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 919739340842.dkr.ecr.eu-north-1.amazonaws.com
+
+echo "Pulling Docker image..."
+docker pull 919739340842.dkr.ecr.eu-north-1.amazonaws.com/swiggy-delivery:latest
+
+echo "Checking for existing container..."
+if [ "$(docker ps -q -f name=delivery_time_pred)" ]; then
+    echo "Stopping existing container..."
+    docker stop delivery_time_pred
+fi
+
+if [ "$(docker ps -aq -f name=delivery_time_pred)" ]; then
+    echo "Removing existing container..."
+    docker rm delivery_time_pred
+fi
+
+echo "Starting new container..."
+docker run -d -p 80:8000 --name delivery_time_pred \
+  -e DAGSHUB_USER_TOKEN=$DAGSHUB_USER_TOKEN \
+  919739340842.dkr.ecr.eu-north-1.amazonaws.com/swiggy-delivery:latest
+
+echo "Container started successfully."
