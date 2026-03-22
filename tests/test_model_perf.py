be4fd271bd8daae848a3031b89744b2b02d8a455
Anmol Chandra Mishra <anmolchandra871@gmail.com>
2026-03-22 12:01:24 +0530

    fix: remove unnecessary preprocessor from test_model_perf

--- tests/test_model_perf.py
+++ tests/test_model_perf.py
@@ -2,13 +2,8 @@ import pytest
 import mlflow
 import dagshub
 import pandas as pd
-import joblib
 from pathlib import Path
 from sklearn.metrics import mean_absolute_error
-
-import os
-os.environ["MLFLOW_TRACKING_USERNAME"] = "anmol-hxgt"
-os.environ["MLFLOW_TRACKING_PASSWORD"] = "41716882be228c494f83e28f51ea10efea8501ed"
 
 # initialize dagshub
 dagshub.init(
@@ -25,7 +20,6 @@ mlflow.set_tracking_uri(
 # -----------------------------
 # Load model from registry
 # -----------------------------
-
 MODEL_NAME = "delivery_time_pred_model"
 STAGE = "Production"
 
@@ -38,12 +32,12 @@ run_id = versions[0].run_id
 model = mlflow.pyfunc.load_model(f"runs:/{run_id}/delivery_time_pred_model")
 
 # -----------------------------
-# Load preprocessor and test data
+# Load test data
 # -----------------------------
 
 root_path = Path(__file__).parent.parent
 test_data_path = root_path / "data" / "processed" / "test_trans.csv"
-preprocessor = joblib.load(root_path / "models" / "preprocessor.joblib")
+
 
 
 @pytest.mark.parametrize(
@@ -57,13 +51,7 @@ def test_model_performance(threshold_error):
     X = df.drop(columns=["time_taken"])
     y = df["time_taken"]
 
-    # preprocess
-    preprocessed = preprocessor.transform(X)
-    feature_names = preprocessor.get_feature_names_out()
-    preprocessed_df = pd.DataFrame(preprocessed, columns=feature_names)
-    preprocessed_df['vehicle_condition'] = preprocessed_df['vehicle_condition'].astype('int64')
-
-    y_pred = model.predict(preprocessed_df)
+    y_pred = model.predict(X)
 
     mean_error = mean_absolute_error(y, y_pred)
 
@@ -74,4 +62,4 @@ def test_model_performance(threshold_error):
         f"Error = {mean_error}, Threshold = {threshold_error}"
     )
 
-    print(" Model passed performance test")
+    print("✅ Model passed performance test")
